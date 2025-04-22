04b5c9c7ed306ebf64b783ea6ed7dcfab434eef6
Omar Soriano <osoriano2@gmail.com>
2025-04-22 07:23:27 +0000

    more resources

--- internal/webserver/flow_webserver.go
+++ internal/webserver/flow_webserver.go
@@ -1,12 +1,15 @@
 package webserver
 
 import (
+	"encoding/json"
 	"fmt"
 	"net"
 	"net/http"
 	"time"
 
 	"github.com/gorilla/websocket"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
@@ -39,9 +42,12 @@ func (s *FlowWebServer) SetupWithManager(mgr ctrl.Manager) error {
 		return fmt.Errorf("webserver failed to listen: %s", err)
 	}
 
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /ws", s.handleWebsocket)
+	mux.HandleFunc("GET /api/v1/namespaces/{name}", s.handleNamespace)
 	err = mgr.Add(&manager.Server{
 		Name:     "jettison server",
-		Server:   newServer(s),
+		Server:   newServer(mux),
 		Listener: serverListener,
 	})
 	if err != nil {
@@ -65,7 +71,7 @@ func (s *FlowWebServer) SetupWithManager(mgr ctrl.Manager) error {
 	return nil
 }
 
-func (s *FlowWebServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (s *FlowWebServer) handleWebsocket(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
 	conn, err := upgrader.Upgrade(w, r, nil)
@@ -74,6 +80,32 @@ func (s *FlowWebServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	s.flowWatcher.register <- &WebConn{ctx: ctx, conn: conn}
+}
+
+func (s *FlowWebServer) handleNamespace(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	w.Header().Set("Content-Type", "application/json")
+
+	namespace := &corev1.Namespace{}
+	name := r.PathValue("name")
+	objectKey := client.ObjectKey{
+		Namespace: name,
+		Name:      name,
+	}
+	err := s.Client.Get(ctx, objectKey, namespace)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			log.FromContext(ctx).Info("namespace not found", "name", name)
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprintf(w, `{"error":"namespace not found"}`)
+			return
+		}
+		log.FromContext(ctx).Error(err, "failed to fetch namespace")
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, `{"error":"failed to fetch namespace"}`)
+		return
+	}
+	json.NewEncoder(w).Encode(namespace)
 }
 
 // Returns a new server with sane defaults. Based on internal package:
