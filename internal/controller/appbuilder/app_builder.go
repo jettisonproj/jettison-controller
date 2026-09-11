d3d95297aaa786000485a4469d88108e26a09ead
Omar <osoriano2@gmail.com>
2026-09-10 20:04:15 -0700

    rename pauseReason (#25)

--- internal/controller/appbuilder/app_builder.go
+++ internal/controller/appbuilder/app_builder.go
@@ -92,7 +92,7 @@ func BuildArgoApps(flowSteps []v1alpha1base.BaseStep) ([]*cdv1.AppProject, []*cd
 			if !appNames[appName] {
 				appNames[appName] = true
 
-				stepEnabled := step.PausedReason == nil || strings.TrimSpace(*step.PausedReason) == ""
+				stepEnabled := step.PauseReason == nil || strings.TrimSpace(*step.PauseReason) == ""
 				application := &cdv1.Application{
 					TypeMeta: applicationTypeMeta,
 					ObjectMeta: metav1.ObjectMeta{
